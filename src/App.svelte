ac281bb663a85d50abe798a59fa855376a0874c1
robots4life <34193243+robots4life@users.noreply.github.com>
2021-02-22 14:21:13 +0100

    difference between local and global CSS for components

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,19 @@
 <script>
 	import Navbar from './Navbar.svelte'
 	import Title from './Title.svelte'
+	import Example from './Example.svelte'
 </script>
 
 <Navbar />
 <Title />
 <Title title="a title coming from the outside of the component Title.svelte logic" />
+
+<Example />
+<p>hello from a span element outside of the Example.svelte component</p>
+
+<style>
+	p {
+		background-color: blue;
+		color: white;
+	}
+</style>
---
+++ src/Example.svelte
@@ -0,0 +1,25 @@
+<p>hello from a span element inside the example.svelte component</p>
+
+<div class="main-title">
+	<h2>example title from inside example.svelte component with the same main-title CSS class</h2>
+</div>
+
+<div class="another-element">
+	<p>
+		another random element from inside the example.svelte component styled with CSS from the
+		gloabal styles found in global.css file
+	</p>
+</div>
+
+<style>
+	p {
+		background-color: rgb(36, 197, 98);
+		color: darkred;
+	}
+	.main-title {
+		color: red;
+	}
+	.another-element p {
+		background-color: var(--primaryColor);
+	}
+</style>
