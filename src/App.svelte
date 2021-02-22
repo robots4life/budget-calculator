1f8086c61a532adeda873e5a580d3567723cb9e9
robots4life <34193243+robots4life@users.noreply.github.com>
2021-02-22 15:06:20 +0100

    iterate over array vs iterate over object

--- src/App.svelte
+++ src/App.svelte
@@ -1,8 +1,57 @@
 <script>
 	import Navbar from './Navbar.svelte'
-	import Title from './Title.svelte'
+	let fruits = ['watermelon', 'lemon', 'kiwi', 'orange', 'banana', 'cherry', 'blueberry']
+	let fruits2 = {
+		1: 'watermelon',
+		2: 'lemon',
+		3: 'kiwi',
+		4: 'orange',
+		5: 'banana',
+		6: 'cherry',
+		7: 'blueberry'
+	}
 </script>
 
 <Navbar />
-<Title />
-<Title title="a title coming from the outside of the component Title.svelte logic" />
+
+<!-- https://stackoverflow.com/a/61089468 -->
+<pre>
+	<code>
+		{`
+{#each fruits as fruit, index}
+	<p>{fruit} | {index}</p>
+{/each}
+		`}
+	</code>
+</pre>
+
+{#each fruits as fruit, index}
+	<p>{fruit} | {index}</p>
+{/each}
+
+<pre>
+	<code>
+		{`
+Iterate over objects		
+https://github.com/sveltejs/svelte/issues/894
+
+{#each Object.entries(fruits2) as [fruit_2_key, fruit_2_value]}
+	<p>{fruit_2_key}: {fruit_2_value} |</p>
+{/each}
+		`}
+</code>
+</pre>
+
+{#each Object.entries(fruits2) as [fruit_2_key, fruit_2_value]}
+	<p>{fruit_2_key}: {fruit_2_value} |</p>
+{/each}
+
+<style>
+	pre {
+		color: white;
+		background-color: black;
+	}
+	p {
+		font-size: 2rem;
+	}
+</style>
