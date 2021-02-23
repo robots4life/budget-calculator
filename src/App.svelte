880305357c39aa469cee8db496ccc1ee3f05074e
robots4life <34193243+robots4life@users.noreply.github.com>
2021-02-23 22:58:23 +0100

    create ExpensesList & pass down expenses.js from App to ExpensesList | export expenses = []

--- src/App.svelte
+++ src/App.svelte
@@ -1,21 +1,14 @@
 <script>
+	// components
 	import Navbar from './Navbar.svelte'
+	import ExpensesList from './ExpensesList.svelte'
+
+	// data
 	import expensesData from './expenses'
 	let expenses = [...expensesData]
 </script>
 
 <Navbar />
-<!-- {#each Object.entries(expenses) as [item.id, item.name, item.amount]}
-	console.log({(item.id, item.name, item.amount)})
-{/each} -->
-
-{#each expenses as item, id}
-	@{@debug item, id}
-	<p>{item.id} : {item.name} : {item.amount}</p>
-{/each}
-
-<style>
-	p {
-		font-size: 2rem;
-	}
-</style>
+<main class="content">
+	<ExpensesList expenses="{expenses}" />
+</main>
---
+++ src/ExpensesList.svelte
@@ -0,0 +1,16 @@
+<script>
+	// components
+	import SectionTitle from './Title.svelte'
+	import SingleExpense from './SingleExpense.svelte'
+	// data
+	export let expenses = []
+</script>
+
+<section>
+	<SectionTitle title="expenses list" />
+	<ul>
+		{#each expenses as expense}
+			<SingleExpense />
+		{/each}
+	</ul>
+</section>
