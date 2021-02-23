fd5b7ffa2a23a50dfb3c4717143469379c09e4b3
robots4life <34193243+robots4life@users.noreply.github.com>
2021-02-23 22:39:08 +0100

    iterate over & debug expenses data

--- src/App.svelte
+++ src/App.svelte
@@ -1,7 +1,21 @@
 <script>
 	import Navbar from './Navbar.svelte'
-	import Title from './Title.svelte'
+	import expensesData from './expenses'
+	let expenses = [...expensesData]
 </script>
 
 <Navbar />
-<Title />
+<!-- {#each Object.entries(expenses) as [item.id, item.name, item.amount]}
+	console.log({(item.id, item.name, item.amount)})
+{/each} -->
+
+{#each expenses as item, id}
+	@{@debug item, id}
+	<p>{item.id} : {item.name} : {item.amount}</p>
+{/each}
+
+<style>
+	p {
+		font-size: 2rem;
+	}
+</style>
