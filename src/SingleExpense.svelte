b4ce3e2112798a1f45834291452ff874a14190fe
robots4life <34193243+robots4life@users.noreply.github.com>
2021-02-24 16:51:04 +0100

    destructuring expense, 3 approaches

--- src/SingleExpense.svelte
+++ src/SingleExpense.svelte
@@ -1,1 +1,13 @@
-<h1>this is a single expense</h1>
+<script>
+	export let index
+	export let id
+	export let name
+	export let amount
+</script>
+
+<div>
+	<h1>index : {index}</h1>
+	<p>id : {id}</p>
+	<p>name : {name}</p>
+	<p>amount : {amount}</p>
+</div>
