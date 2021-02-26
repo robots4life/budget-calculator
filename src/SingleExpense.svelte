36da2a582c5d9955af68a56f0ddab88f082454c5
robots4life <34193243+robots4life@users.noreply.github.com>
2021-02-26 13:45:32 +0100

    prop drilling -  passing properties down the component tree

--- src/SingleExpense.svelte
+++ src/SingleExpense.svelte
@@ -4,6 +4,13 @@
 	export let name
 	export let amount
 
+	// finally we arrived at the 3rd deepest component in the component tree
+	// here we can now use the removeSingleExpense(id) function together
+	// with the exported removeSingleExpense property
+	// this leaves us with a trail of properties through all 3 components
+	// from App to ExpensesList to SingleExpense
+	export let removeSingleExpense
+
 	let displayExpenseData = false
 
 	function toggleDisplayExpenseData() {
@@ -16,7 +23,7 @@
 		<h2>
 			{name}
 			<button class="amount-btn" on:click="{toggleDisplayExpenseData}">
-				<i class="fas fa-caret-down" id="index-{index}"></i>
+				<i class="fas fa-caret-down" index="index-{index}"></i>
 			</button>
 		</h2>
 		{#if displayExpenseData}
@@ -27,10 +34,25 @@
 		{/if}
 	</div>
 	<div class="expense-buttons">
+		<!-- edit button -->
 		<button class="expense-btn edit-btn">
 			<i class="fas fa-pen"></i>
 		</button>
-		<button class="expense-btn delete-btn">
+
+		<!-- delete button -->
+		<!-- even though on:click="{removeSingleExpense(id)}" works different in the tutorial
+		because of different Svelte versions
+		https://svelte.dev/repl/075fd497f3bb45199c62775bf643379b?version=3.32.3
+		vs
+		https://svelte.dev/repl/075fd497f3bb45199c62775bf643379b?version=3.0.0
+		it is  recommended to call the property/function exactly like this
+		of course
+		on:click="{(element) => {
+					console.log(element)
+					removeSingleExpense(id)
+				}}"
+		also works fine -->
+		<button class="expense-btn delete-btn" on:click="{removeSingleExpense(id)}">
 			<i class="fas fa-trash"></i>
 		</button>
 	</div>
